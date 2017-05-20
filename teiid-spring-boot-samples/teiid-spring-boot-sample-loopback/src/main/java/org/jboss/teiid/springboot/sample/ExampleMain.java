af634943244936710c52ba44cac7439c50c6265f
kylin <kylinsoong.1214@gmail.com>
2017-05-20 23:27:34 +0800

    add loopback example

--- teiid-spring-boot-samples/teiid-spring-boot-sample-loopback/src/main/java/org/jboss/teiid/springboot/sample/ExampleMain.java
+++ teiid-spring-boot-samples/teiid-spring-boot-sample-loopback/src/main/java/org/jboss/teiid/springboot/sample/ExampleMain.java
@@ -23,7 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleMain {
 
     public static void main(String[] args) {
-//        args = new String[]{"src/main/resources/empty-vdb.xml"};
+        args = new String[]{"src/main/resources/loopback-vdb.xml"};
         SpringApplication.run(ExampleMain.class, args);
     }
 
