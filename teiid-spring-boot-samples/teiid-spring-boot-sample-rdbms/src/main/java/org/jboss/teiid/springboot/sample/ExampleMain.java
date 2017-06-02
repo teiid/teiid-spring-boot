b3e0082416dc284e40de7a39b3ada011ab1d69a0
kylin <kylinsoong.1214@gmail.com>
2017-06-02 14:51:04 +0800

    TEIID-4940: finish use Spring datasource

--- teiid-spring-boot-samples/teiid-spring-boot-sample-rdbms/src/main/java/org/jboss/teiid/springboot/sample/ExampleMain.java
+++ teiid-spring-boot-samples/teiid-spring-boot-sample-rdbms/src/main/java/org/jboss/teiid/springboot/sample/ExampleMain.java
@@ -16,13 +16,37 @@
 
 package org.jboss.teiid.springboot.sample;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class ExampleMain {
+    
+    @Bean
+    public Set<String> connectionFactoryNames() {
+        Set<String> names = new HashSet<>();
+        names.add("account-ds");
+        return names;
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean(name = "account-ds")
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     public static void main(String[] args) throws Exception {
+        args = new String[]{"src/main/resources/portfolio-vdb.xml"};
         SpringApplication.run(ExampleMain.class, args);
     }
 }
