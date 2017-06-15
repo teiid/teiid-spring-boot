e7d3ff9c327367d35ddf89915fbc8af1bf98d5e1
kylin <kylinsoong.1214@gmail.com>
2017-06-15 18:39:25 +0800

    fix the test

--- starter-test/src/test/java/org/teiid/autoconfigure/JDBCUtils.java
+++ starter-test/src/test/java/org/teiid/autoconfigure/JDBCUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.teiid.springboot;
+package org.teiid.autoconfigure;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
