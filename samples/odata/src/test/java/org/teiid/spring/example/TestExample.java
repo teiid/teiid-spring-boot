e9695b8c97a7d4f53b99740bde2e9881a0a919c2
Ramesh Reddy <rareddy@jboss.org>
2017-09-19 12:39:09 -0500

    Adding a test case for OData

--- samples/odata/src/test/java/org/teiid/spring/example/TestExample.java
+++ samples/odata/src/test/java/org/teiid/spring/example/TestExample.java
@@ -1,5 +1,8 @@
 package org.teiid.spring.example;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +13,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {Application.class, TestConfiguration.class, RestTemplate.class})
+@SpringBootTest(classes = { Application.class, TestConfiguration.class, RestTemplate.class })
 public class TestExample {
 	@Autowired
 	RestTemplate web;
 	
     @Test
     public void test() {
-    	/*
     	ResponseEntity<String> response
     	  = web.getForEntity("http://localhost:8080/$metadata", String.class);
     	assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-    	*/
     }
 
 }
