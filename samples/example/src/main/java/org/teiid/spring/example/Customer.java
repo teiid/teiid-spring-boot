669e57e332bfad7a45e86ec410d2b46bee91cf6f
Ramesh Reddy <rareddy@jboss.org>
2020-01-30 17:19:54 -0600

    TEIIDSB-166: updating example

---
+++ samples/example/src/main/java/org/teiid/spring/example/Customer.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teiid.spring.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.teiid.spring.annotations.SelectQuery;
+
+/**
+ * This entity is showing a JOIN between table in H2 database called product and
+ * text file bases entity StockPrice.
+ */
+@Entity
+@SelectQuery("SELECT id, FIRSTNAME, LASTNAME FROM africa.Customer"
+        + " UNION ALL"
+        + " SELECT id, FIRSTNAME, LASTNAME FROM europe.Customer")
+public class Customer {
+
+    @Id
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Customer other = (Customer) obj;
+        if (firstName == null) {
+            if (other.firstName != null) {
+                return false;
+            }
+        } else if (!firstName.equals(other.firstName)) {
+            return false;
+        }
+        if (id != other.id) {
+            return false;
+        }
+        if (lastName == null) {
+            if (other.lastName != null) {
+                return false;
+            }
+        } else if (!lastName.equals(other.lastName)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ samples/example/src/main/resources/data-europe.sql
@@ -0,0 +1,27 @@
+--
+-- Copyright (C) 2016 Red Hat, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1015,'Jack','Corby');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1019,'Robin','Evers');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1020,'Lloyd','Abercrombie');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1021,'Scott','Watters');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1022,'Sandra','King');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1027,'Maryanne','Peters');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1034,'Corey','Snyder');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1035,'Henry','Thomas');
+INSERT INTO CUSTOMER (ID,FIRSTNAME,LASTNAME) VALUES (1036,'James','Drew');
+
+commit;
+
