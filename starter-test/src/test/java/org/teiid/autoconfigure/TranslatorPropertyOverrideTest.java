f56073ed94d608d7ee8ac35655298fa2aa64bffe
Ramesh Reddy <rareddy@jboss.org>
2020-06-01 08:59:24 -0500

    ENTESB-13951: Setting the translator override properties that are set as data source properties on to the translator (#235)

---
+++ starter-test/src/test/java/org/teiid/autoconfigure/TranslatorPropertyOverrideTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teiid.autoconfigure;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.teiid.spring.autoconfigure.TeiidAutoConfiguration;
+import org.teiid.spring.configuration.TestConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {TeiidAutoConfiguration.class, TestConfiguration.class})
+@DirtiesContext
+@TestPropertySource("classpath:translator-override.properties")
+public class TranslatorPropertyOverrideTest {
+
+    @Autowired
+    private DataSource datasource;
+
+    @Test
+    public void testCustomTranslator() throws SQLException {
+        Connection conn = datasource.getConnection();
+        Statement stmt = conn.createStatement();
+        assertNotNull(stmt);
+        ResultSet rs = stmt.executeQuery("SELECT * FROM accounts.mytable");
+        assertNotNull(rs);
+        assertTrue(rs.next());
+        assertEquals("one", rs.getString(1));
+        assertTrue(rs.next());
+        assertEquals("overridden", rs.getString(1));
+
+    }
+
+}
