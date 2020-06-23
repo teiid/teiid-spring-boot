cc32d41918c2ca7e6eaa2049211eb14868e09c1e
shawkins <shawkins@redhat.com>
2020-06-23 14:56:29 -0400

    TEIIDSB-206 correcting the translator name

--- data/hdfs/src/main/java/org/teiid/spring/data/hdfs/HdfsConnectionFactory.java
+++ data/hdfs/src/main/java/org/teiid/spring/data/hdfs/HdfsConnectionFactory.java
@@ -25,7 +25,7 @@ import org.teiid.spring.data.ConnectionFactoryConfiguration;
 
 @ConnectionFactoryConfiguration(
         alias = "hdfs",
-        translatorName = "hdfs"
+        translatorName = "file"
 )
 public class HdfsConnectionFactory implements BaseConnectionFactory<HdfsConnection> {
 
