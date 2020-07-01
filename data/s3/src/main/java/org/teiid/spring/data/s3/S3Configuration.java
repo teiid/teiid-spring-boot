de6df7a06acd49e7af3fdb05351555cb02e0fea4
Aditya Manglam Sharma <aditya300899@gmail.com>
2020-07-01 23:16:58 +0530

    TEIIDSB 211 spring data s3 (#267)
    * adding support for s3
    
    * adding test class and doing refinements
    
    * commiting to review test class and other refinements
    
    * adding the test class
    
    * doing clean-up in test class

---
+++ data/s3/src/main/java/org/teiid/spring/data/s3/S3Configuration.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags and
+ * the COPYRIGHT.txt file distributed with this work.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.teiid.spring.data.s3;
+
+public class S3Configuration {
+
+    private String accessKey;
+    private String secretKey;
+    private String bucket;
+    private String region = "US-EAST-1";
+    private String sseAlgorithm = "AES256";
+    private String sseKey = null;   // base64-encoded key
+    private String endpoint = "https://s3.amazonaws.com";
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getSseAlgorithm() {
+        return sseAlgorithm;
+    }
+
+    public void setSseAlgorithm(String sseAlgorithm) {
+        this.sseAlgorithm = sseAlgorithm;
+    }
+
+    public String getSseKey() {
+        return sseKey;
+    }
+
+    public void setSseKey(String sseKey) {
+        this.sseKey = sseKey;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+}
---
+++ data/s3/src/main/java/org/teiid/spring/data/s3/S3ConnectionFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags and
+ * the COPYRIGHT.txt file distributed with this work.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.teiid.spring.data.s3;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.teiid.spring.data.BaseConnectionFactory;
+import org.teiid.spring.data.ConnectionFactoryConfiguration;
+import org.teiid.translator.TranslatorException;
+
+import java.io.IOException;
+
+@ConnectionFactoryConfiguration(
+        alias = "s3",
+        translatorName = "file"
+)
+public class S3ConnectionFactory implements BaseConnectionFactory<S3Connection> {
+
+    private volatile AmazonS3 s3Client;
+    private S3Configuration s3Config;
+
+    public S3ConnectionFactory(S3Configuration s3Config) {
+        this.s3Config = s3Config;
+    }
+
+    @Override
+    public S3Connection getConnection() throws Exception {
+        if(s3Config.getBucket() == null) {
+            throw new TranslatorException("Bucket can't be null.");
+        }
+        if(s3Config.getAccessKey() == null) {
+            throw new TranslatorException("Access key can't be null.");
+        }
+        if(s3Config.getSecretKey() == null) {
+            throw new TranslatorException("Secret key can't be null.");
+        }
+        AWSCredentials credentials = new BasicAWSCredentials(s3Config.getAccessKey(), s3Config.getSecretKey());
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
+        s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Config.getEndpoint(), s3Config.getRegion()))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+        return new S3Connection(this.s3Config, this.s3Client);
+    }
+
+    @Override
+    public void close() throws IOException {
+        s3Client.shutdown();
+    }
+}
