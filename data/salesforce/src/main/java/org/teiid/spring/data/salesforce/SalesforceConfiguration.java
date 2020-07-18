378545780f90c22cc761e79b114be77449a91e31
renat.eskenin@veeam.com <renat.eskenin@veeam.com>
2020-07-18 15:09:10 +0200

    Revert "Add auth urls to properties"
    This reverts commit b64c8f6819d4c79ae3c36eaee262a11d3ec32362.

--- data/salesforce/src/main/java/org/teiid/spring/data/salesforce/SalesforceConfiguration.java
+++ data/salesforce/src/main/java/org/teiid/spring/data/salesforce/SalesforceConfiguration.java
@@ -20,128 +20,111 @@ package org.teiid.spring.data.salesforce;
 import org.springframework.web.client.RestTemplate;
 
 public class SalesforceConfiguration implements org.teiid.salesforce.SalesforceConfiguration {
-	private String url = "https://login.salesforce.com/services/Soap/u/45.0";
-	private String authorizeUrl = "https://login.salesforce.com/services/oauth2/authorize";
-	private String tokenUrl = "https://login.salesforce.com/services/oauth2/token";
-	private Long requestTimeout = Long.MAX_VALUE;
-	private Long connectTimeout = Long.MAX_VALUE;
-	private Integer pollingInterval = 500;
-	private String clientId;
-	private String clientSecret;
-	private String username;
-	private String password;
-	private String refreshToken;
-	private RestTemplate restTemplate = new RestTemplate();
+    private String url = "https://login.salesforce.com/services/Soap/u/45.0";
+    private Long requestTimeout = Long.MAX_VALUE;
+    private Long connectTimeout = Long.MAX_VALUE;
+    private Integer pollingInterval = 500;
+    private String clientId;
+    private String clientSecret;
+    private String username;
+    private String password;
+    private String refreshToken;
+    private RestTemplate restTemplate = new RestTemplate();
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public SalesforceOAuth2Template getOAuth2Template() {
-		SalesforceOAuth2Template template = new SalesforceOAuth2Template(clientId, clientSecret, authorizeUrl,
-				tokenUrl);
-		template.setUseParametersForClientAuthentication(true);
-		return template;
-	}
+    public SalesforceOAuth2Template getOAuth2Template() {
+        SalesforceOAuth2Template template = new SalesforceOAuth2Template(clientId, clientSecret,
+                "https://login.salesforce.com/services/oauth2/authorize",
+                "https://login.salesforce.com/services/oauth2/token");
+        template.setUseParametersForClientAuthentication(true);
+        return template;
+    }
 
-	@Override
-	public String getURL() {
-		return url;
-	}
+    @Override
+    public String getURL() {
+        return url;
+    }
 
-	@Override
-	public String getUsername() {
-		return username;
-	}
+    @Override
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String name) {
-		this.username = name;
-	}
+    public void setUsername(String name) {
+        this.username = name;
+    }
 
-	@Override
-	public Long getRequestTimeout() {
-		return requestTimeout;
-	}
+    @Override
+    public Long getRequestTimeout() {
+        return requestTimeout;
+    }
 
-	public void setRequestTimeout(Long requestTimeout) {
-		this.requestTimeout = requestTimeout;
-	}
+    public void setRequestTimeout(Long requestTimeout) {
+        this.requestTimeout = requestTimeout;
+    }
 
-	@Override
-	public Long getConnectTimeout() {
-		return connectTimeout;
-	}
+    @Override
+    public Long getConnectTimeout() {
+        return connectTimeout;
+    }
 
-	public void setConnectTimeout(Long connectTimeout) {
-		this.connectTimeout = connectTimeout;
-	}
+    public void setConnectTimeout(Long connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
 
-	public Integer getPollingInterval() {
-		return pollingInterval;
-	}
+    public Integer getPollingInterval() {
+        return pollingInterval;
+    }
 
-	public void setPollingInterval(Integer pollingInterval) {
-		this.pollingInterval = pollingInterval;
-	}
+    public void setPollingInterval(Integer pollingInterval) {
+        this.pollingInterval = pollingInterval;
+    }
 
-	public String getClientId() {
-		return clientId;
-	}
+    public String getClientId() {
+        return clientId;
+    }
 
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
 
-	public String getClientSecret() {
-		return clientSecret;
-	}
+    public String getClientSecret() {
+        return clientSecret;
+    }
 
-	public void setClientSecret(String clientSecret) {
-		this.clientSecret = clientSecret;
-	}
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
 
-	@Override
-	public String getPassword() {
-		return password;
-	}
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public RestTemplate getRestTemplate() {
-		return restTemplate;
-	}
+    public RestTemplate getRestTemplate() {
+        return restTemplate;
+    }
 
-	public void setRestTemplate(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-	public String getRefreshToken() {
-		return refreshToken;
-	}
+    public String getRefreshToken() {
+        return refreshToken;
+    }
 
-	public void setRefreshToken(String refreshToken) {
-		this.refreshToken = refreshToken;
-	}
-
-	public String getAuthorizeUrl() {
-		return authorizeUrl;
-	}
-
-	public void setAuthorizeUrl(String authorizeUrl) {
-		this.authorizeUrl = authorizeUrl;
-	}
-
-	public String getTokenUrl() {
-		return tokenUrl;
-	}
-
-	public void setTokenUrl(String tokenUrl) {
-		this.tokenUrl = tokenUrl;
-	}
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
