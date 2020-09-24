50b37e660e581397b99cf133f35bb50eaf7dba70
Ramesh Reddy <rareddy@jboss.org>
2020-09-24 14:37:51 -0500

    TEIIDSB-219: Adding a way to provide a VDB with multiple DDL files using the DDL-FILE metadata repository

---
+++ samples/vdb-split/src/main/resources/data-mydb.sql
@@ -0,0 +1,24 @@
+--
+-- Copyright (C) 2016 Red Hat, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+INSERT INTO CUSTOMER (ID,SSN,NAME) VALUES (10, 'CST01002','Joseph Smith');
+INSERT INTO CUSTOMER (ID,SSN,NAME) VALUES (11, 'CST01003','Nicholas Ferguson');
+INSERT INTO CUSTOMER (ID,SSN,NAME) VALUES (12, 'CST01004','Jane Aire');
+
+INSERT INTO ADDRESS (ID, STREET, ZIP, CUSTOMER_ID) VALUES (10, 'Main St', '12345', 10);
+
+INSERT INTO IDGENERATOR(IDKEY, IDVALUE) VALUES ('customer', 100);
+commit;
+
---
+++ samples/vdb-split/src/main/resources/schema-mydb.sql
@@ -0,0 +1,46 @@
+--
+-- Copyright (C) 2016 Red Hat, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+SET SCHEMA myschema;
+
+DROP TABLE IF EXISTS CUSTOMER;
+CREATE TABLE CUSTOMER
+(
+   ID bigint,
+   SSN char(25),
+   NAME varchar(64),
+   CONSTRAINT CUSTOMER_PK PRIMARY KEY(ID)
+);
+
+DROP TABLE IF EXISTS ADDRESS;
+CREATE TABLE ADDRESS
+(
+   ID bigint,
+   STREET char(25),
+   ZIP char(10),
+   CUSTOMER_ID bigint,
+   CONSTRAINT ADDRESS_PK PRIMARY KEY(ID),
+   CONSTRAINT CUSTOMER_FK FOREIGN KEY (CUSTOMER_ID) REFERENCES CUSTOMER (ID)
+);
+
+DROP TABLE IF EXISTS IDGENERATOR;
+CREATE TABLE IDGENERATOR 
+(
+    IDKEY char(10) NOT NULL,
+    IDVALUE bigint NOT NULL
+);
+-- H2 even though you ask to start at 200, in this example it always starting at 1
+CREATE SEQUENCE IF NOT EXISTS customer_seq START WITH 200 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS addr_seq START WITH 200 INCREMENT BY 1;
