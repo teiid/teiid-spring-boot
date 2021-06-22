7c4e118417c50ccb8f01956ef7e4a2af7d01d6f9
Ramesh Reddy <rareddy@jboss.org>
2021-06-22 17:41:32 -0500

    correcting the loading of the vdb when supplied in multiple .ddl or .sql files (#318)

--- samples/rest/src/test/java/org/teiid/spring/example/TestExample.java
+++ samples/rest/src/test/java/org/teiid/spring/example/TestExample.java
@@ -18,6 +18,7 @@ package org.teiid.spring.example;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = { Application.class, TestConfiguration.class})
+//turned off due to dependency on external API for Employee service but one can run this fine
+@Ignore
 public class TestExample {
 
     @Autowired
